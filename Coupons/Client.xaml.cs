ecc76e21a7c927db23a14273925a4215317c6aba
Benjano <nir4benjano@gmail.com>
2015-04-26 17:58:47 +0300

    dhfg

--- Coupons/Client.xaml.cs
+++ Coupons/Client.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CouponsApplication
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Client.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Client : Window
     {
-        public MainWindow()
+        public Client()
         {
             InitializeComponent();
-        }
-
-        private void Login_Click(object sender, RoutedEventArgs e)
-        {
-            Client form = new Client();
-            form.Show();
-            this.Close();
         }
     }
 }
