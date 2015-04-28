5aa43c2758333d560bd18952c57c98cf54633a0c
Benjano <nir4benjano@gmail.com>
2015-04-28 22:51:45 +0300

    a

--- Coupons/GUI/CreateClientWindow.xaml.cs
+++ Coupons/GUI/CreateClientWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Device.Location;
 
 namespace Coupons
 {
@@ -25,6 +26,7 @@ namespace Coupons
 
         MainWindow mMainWindow;
         ClientBL mClientBL;
+        GeoCoordinateWatcher mGeoWatcher;
 
         public CreateClientWindow()
         {
@@ -34,6 +36,8 @@ namespace Coupons
             cbGender.ItemsSource = Enum.GetValues(typeof(Gender));
             cbGender.SelectedIndex = 0;
             dpBirthDate.SelectedDate = DateTime.Today;
+            mGeoWatcher = new GeoCoordinateWatcher();
+            mGeoWatcher.Start();
         }
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
@@ -45,10 +49,11 @@ namespace Coupons
             String phone = tbPhone.Text;
             Gender gender = (Gender) cbGender.SelectedItem;
             DateTime birthdate = (DateTime) dpBirthDate.SelectedDate;
+            String location = mGeoWatcher.Position.Location.ToString();
 
             if (username.Length > 2 && password.Length > 5 && mail.Length > 7 && phone.Length > 6 && birthdate != null)
             {
-                mClientBL.insertNewClient(username, password, mail, phone, birthdate, gender, "");
+                mClientBL.insertNewClient(username, password, mail, phone, birthdate, gender, location);
             }
             else
             {
