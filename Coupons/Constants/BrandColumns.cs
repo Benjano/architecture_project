c1db0a917cfb10a3d1f5a9a1a2e48fcb7da74db5
avivasi <aviv22a@gmail.com>
2015-06-22 23:48:03 +0300

    h

---
+++ Coupons/Constants/BrandColumns.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coupons.Constants
+{
+    public class BrandColumns
+    {
+        public static String ID = "Id";
+        public static String NAME = "Name";
+    }
+
+}
